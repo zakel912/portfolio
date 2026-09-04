567c9d1d45c7fb58b88d2610fa5d1722c0de4005
Zakaria <153775450+zakel912@users.noreply.github.com>
2026-09-04 18:43:38 +0200

    Update repository name for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isGithubPages = process.env.GITHUB_ACTIONS === "true";
-const repoName = "zakaria_el_mrani_portfolio.github.dev";
+const repoName = "portfolio";
 
 const basePath = isGithubPages ? `/${repoName}` : "";
 
